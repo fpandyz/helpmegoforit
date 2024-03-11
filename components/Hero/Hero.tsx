1814f1d288184761ff513fbf2746336806f3fb53
Dmitriy Myakotin <demon4301@gmail.com>
2024-03-11 08:23:21 +0500

    chore(Hero): #86940krgk: add style

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,17 +1,17 @@
+import Image from 'next/image';
+
 export default function Hero() {
   return (
     <section className="hero">
       <div className="container hero__wrapper">
         <div className="hero__content">
           <picture className="hero__image">
-
             <source
               srcSet="images/hero-image.png"
               media="(min-width: 768px)"
             />
             <img srcSet="images/hero-image-mobile.png" alt="heroImage" />
           </picture>
-
           <h1 className="hero__title">
             Fron
             <strong className="hero__title--bold">TDD</strong>
@@ -27,6 +27,13 @@ export default function Hero() {
             Frontend Developers
           </h2>
         </div>
+        <Image
+          className="hero__arrow"
+          src="images/hero-arrow.svg"
+          width={17}
+          height={64}
+          alt="heroArrow"
+        />
       </div>
     </section>
   );
