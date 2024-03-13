906515ed6b0a81c6fff6da5c2194a4a4d612eaf9
Dmitriy Myakotin <demon4301@gmail.com>
2024-03-13 08:29:25 +0500

    refactor: #86941cd7h: move the card to a separate component

--- components/Learn/Learn.tsx
+++ components/Learn/Learn.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image';
 import IconTraces from '../../public/images/learn-traces.svg';
 import IconHeart from '../../public/images/learn-heart.svg';
 import IconBox from '../../public/images/learn-box.svg';
 import IconHand from '../../public/images/learn-hand.svg';
 import IconQuestions from '../../public/images/learn-question.svg';
 import { Author } from './components/Author/Author';
+import { Card } from '../Card/Card';
 
 const learnCards = [
   {
@@ -51,16 +51,11 @@ export function Learn() {
               icon,
             },
           ) => (
-            <div key={description} className="learn__card">
-              <span className="learn__description text-type-3">{description}</span>
-              <span className="learn__icon">
-                <Image
-                  src={icon}
-                  alt="cardIcon"
-                  fill
-                />
-              </span>
-            </div>
+            <Card
+              key={description}
+              description={description}
+              icon={icon}
+            />
           ))}
         </div>
         <picture className="learn__background">
