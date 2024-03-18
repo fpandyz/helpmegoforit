2f90d366e74c479416e1d7ce598c0cc73ff46dbd
Anastasia Saraeva <asaraeva@tourmalinecore.com>
2024-03-18 17:23:35 +0500

    refactor(Author): #869423782: rename and replace styles

--- components/Learn/components/Author/Author.tsx
+++ components/Learn/components/Author/Author.tsx
@@ -7,13 +7,13 @@ export function Author() {
   return (
     <div className="author">
 
-      <div className="author__citation author__citation--mobile">
-        <div className="author__quotes" role="img" />
-        <h2 className="author__citation-text title-type-2">
-          <span className="author__citation-quotes">«</span>
+      <div className="author__citation">
+        <div className="author__img" role="img" />
+        <h2 className="title-type-2 author__text">
+          <span className="author__quotes">«</span>
           В начале карьеры мы&nbsp;понимаем, что&nbsp;делаем ерунду,
           но&nbsp;мы&nbsp;не&nbsp;знаем, как&nbsp;делать лучше
-          <span className="author__citation-quotes">»</span>
+          <span className="author__quotes">»</span>
         </h2>
       </div>
 
@@ -24,9 +24,9 @@ export function Author() {
             alt="Автор"
           />
         </span>
-        <div className="author__text">
-          <p className="author__name text-type-2">Александр Шинкарев</p>
-          <p className="author__description text-type-2">
+        <div className="author__wrapper">
+          <p className="text-type-2 author__name ">Александр Шинкарев</p>
+          <p className="text-type-2 author__description">
             Автор курса, разработчик с 12-летним опытом, фанат&nbsp;
             <a className="author__link" href={LINK} target="_blank">Терри&nbsp;Пратчетта</a>
           </p>
