dbf0a686e3283ee912f2942395bbd2dc0522bfa1
akovylyaeva <akovylyaeva@tourmalinecore.com>
2024-03-29 11:30:45 +0500

    feat: #869445r4m: replace logo and author image

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import IconGit from '../../public/images/icon-git.svg';
 import IconTg from '../../public/images/icon-tg.svg';
 import IconVk from '../../public/images/icon-vk.svg';
-import Logo from '../../public/images/logo.webp';
+import Logo from '../../public/images/logo.png';
 import Question from '../../public/images/question.webp';
 
 export function Footer() {
