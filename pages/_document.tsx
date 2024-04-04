04a979eba58979533173174d09f37c6a90d23a53
akovylyaeva <akovylyaeva@tourmalinecore.com>
2024-04-04 15:39:05 +0500

    feat: #869445r4m: add favicons for different browsers

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,6 +15,15 @@ export default function Document() {
     <Html lang="ru">
       <Head>
         <meta name="yandex-verification" content="d7c2b9f9a4891879" />
+
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#ef0c85" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
+
         <link rel="preconnect" href="https://mc.yandex.ru" />
       </Head>
       <title>Курсы фронтенд разработчика бесплатно с нуля</title>
